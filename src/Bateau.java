a004afcf78c6de4a65e57bcfacdf71fe58ad42d3
Rangivaru Salem <salem.rangivaru@gmail.com>
2016-02-02 15:39:37 +0100

    modification de la classe bateau

--- src/Bateau.java
+++ src/Bateau.java
@@ -3,68 +3,52 @@ public class Bateau {
 	
 	private int tailleBat;
 	private String sensBateau;
-	private int caseTouche;//compteur de case du bateau touch�
+	private int caseTouche;//compteur de case du bateau touché
 	private int x,y;
 	private Case cases[];
+	
+	public Bateau() { 
+	
+	}
 	
 	public Bateau(int taille) {
 		this.tailleBat = taille;
 		cases = new Case[taille]; 
 	
 	}
-	/**
-	 * Permet de retourner la case souhait�
-	 * @param pos
-	 * @return
-	 */
+	
 	public Case getCase(int pos){
 		return cases[pos];
 	}
-	/**
-	 * Permet de modifer une case
-	 * @param pos
-	 * @param cases
-	 */
+	/*
+	 * Permet de modifier une case
+	 * */
 	public void setCase(int pos, Case cases){
 		this.cases[pos]= cases ;
 	}
 
-	/**
-	 * Retourne la taille du bateau
-	 * @return
-	 */
+	
 	public int getTailleBat(){
 		return tailleBat;
 	}
-	/**
-	 * Dans le cas ou une case du bateau est touch�
-	 */
+	
 	public void bateauTouche() {
 		caseTouche++;
-		System.out.println("Touch�");
+		System.out.println("Touché");
 	}
 	
-	/**
-	 * Retourne vrai si le bateau n'a plus de case
-	 * @return
-	 */
+	
 	public boolean estCoule(){
 		if(caseTouche == tailleBat){
 			return true;
 		}
 		else{ return false;}
 	}
-	/**
-	 * Permet de positionner le bateau verticalement ou horizontalement
-	 * @param sensBateau
-	 */
+	
 	public void setSensBateau(String sensBateau){
 		this.sensBateau = sensBateau;
 	}
-	/**
-	 * Permet de connaitre le sens du bateau
-	 * @return 
-	 */
+	
 	public String getSensBateau(){
 		return sensBateau;
 	}
--- src/Case.java
+++ src/Case.java
@@ -2,8 +2,8 @@
 public class Case {
 	protected static final int VIDE = 0; // case vide mais pas de tir deja fait
 	protected static final int OCCUPE = 1; // bateau present
-	protected static final int TOUCHE = 2; // bateau touch�
-	protected static final int MANQUE = 3; // bateau rat� 
+	protected static final int TOUCHE = 2; // bateau touché
+	protected static final int MANQUE = 3; // bateau raté
 	
 	
 	private int x,y;
@@ -15,38 +15,23 @@ public class Case {
 		this.y = y;
 		etatCase = VIDE;
 	}
-	/**
-	 * Permet de savoir si la case a etait touch�
-	 * @return
-	 */
+	
 	public boolean estTouche(){
 		return caseTouche;
 	}
-	/** 
-	 * Permet de connaitre l'�tat de la case
-	 * @return
-	 */
+	
 	public int getEtatCase(){
 		return etatCase;
 	}
-	/**
-	 * Permet de modifier l'etat de la case
-	 * @param etatCase
-	 */
+	
 	public void setEtatCase(int etatCase){
 		this.etatCase = etatCase;
 	}
-	/**
-	 * Permet d'avoir la position x de la case
-	 * @return
-	 */
+	
 	public int getX(){
 		return x; 
 	}
-	/**
-	 * Permet d'avoir la position y de la case
-	 * @return
-	 */
+	
 	public int getY(){
 		return y; 
 	}
